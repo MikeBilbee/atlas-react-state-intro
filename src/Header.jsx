4cec7d594ca8d630cc71e84f6b743d93b3a57f84
MikeBilbee <cmbilbee@gmail.com>
2025-01-09 18:22:26 -0600

    5.1 Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,17 @@
+// src/Header.jsx
+import { useContext } from "react";
 import logo from "./assets/logo.png";
+import { EnrollmentContext } from "./App";
 
 export default function Header() {
-  return (
-    <div className="header">
-      <img src={logo} alt="logo" className="logo" />
-      <div className="enrollment">Classes Enrolled: 1</div>
-    </div>
-  );
+    const { enrolledCourses } = useContext(EnrollmentContext);
+
+    return (
+        <div className="header">
+            <img src={logo} alt="logo" className="logo" />
+            <div className="enrollment">
+                Classes Enrolled: {enrolledCourses.length}
+            </div>
+        </div>
+    );
 }
